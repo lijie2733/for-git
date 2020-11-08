b861787cf53d5891871f08a0931409538b86f093
lijie2733 <2315673858@qq.com>
2020-11-08 22:48:50 +0800

    complete sort_value

--- linklist/main.cpp
+++ linklist/main.cpp
@@ -30,26 +30,26 @@ int linklist_and_func::print_linklist(){
     }
 }
 int linklist_and_func::sort_value(){
-    linklist* p=L->next;//p指针指向头元素
-    linklist* t=p->next;//t指针指向下一个元素
+    linklist* p=L->next;//p指针指向首元素
+    linklist* t=p->next;//t指针指向第二个元素
     int i=L->data;
-    int ox=1;    
+    int ox=1;
     while(i--&&ox){
-        while(t!=NULL){
+        ox=0;
+        for(int j=1;j<i;j++){
             int temp;
             if(p->data>t->data){
             temp=p->data;
             p->data=t->data;
             temp->data=temp;
+            ox=1;
             }
             p=p->next;
             t=t->next;}
             }
-        p=L;
-        
-    }
-    
-
+        p=L->next;
+        t=p->next;
+    }    
 int main(void){
     int n;
     cout<<"please tell me the length of your linklist";
