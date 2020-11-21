b4b21a4855133bbec53078974d6b6b8da17f7551
lijie2733 <2315673858@qq.com>
2020-11-21 11:37:26 +0800

    add stack.h not finished

---
+++ stack/stack.h
@@ -0,0 +1,13 @@
+#pragma once
+#define SIZE_INIT 15
+class stack{
+    public:
+    int size=SIZE_INIT;
+    int[size] p;
+}
+class stack_and_func{
+    public:
+    int init_stack{
+        new stack* t;
+    }
+}
